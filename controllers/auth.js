977051b7805189f841899009ea076f42f3fbfed2
danala <greatprotectionwallet@gmail.com>
2022-10-23 14:56:46 -0600

    Login route and validation added

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,3 +37,36 @@ exports.signup = (req, res, next) => {
             next(err)
         })
 }
+
+exports.login = (req, res, next) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    User.findOne({ email: email })
+        .then((user) => {
+            if (!user) {
+                const error = new Error(
+                    'A user with this email & password combination was not found.'
+                )
+                error.statusCode = 401
+                throw error
+            }
+            loadedUser = user
+            return bcrypt.compare(password, user.password)
+        })
+        .then((isEqual) => {
+            if (!isEqual) {
+                const error = new Error(
+                    'A user with this email & password combination was not found.'
+                )
+                error.statusCode = 401
+                throw error
+            }
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+}
