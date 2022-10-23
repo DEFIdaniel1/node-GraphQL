eb05df73ffd931ec81789c2ccd9aa36f1f639107
danala <greatprotectionwallet@gmail.com>
2022-10-23 13:57:08 -0600

    Auth controller and validation added for new-user signup.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,30 @@
 const express = require('express')
+const { body } = require('express-validator')
 const router = express.Router
 
-router.put('/signup')
+const User = require('../models/user')
+const authController = require('../controllers/auth')
+
+router.put(
+    '/signup',
+    [
+        // Validation
+        body('email')
+            .isEmail()
+            .withMessage('Please enter a valid email.')
+            // Add custom validation requirement. Checking our DB if user already exists
+            .custom((value, { req }) => {
+                return User.findOne({ email: value }).then((matchingUser) => {
+                    if (matchingUser) {
+                        return Promise.reject('Email already exists')
+                    }
+                })
+            })
+            .normalizeEmail(),
+        body('password').trim().isLength({ min: 7 }),
+        body('name').trim().not().isEmpty(),
+    ],
+    authController.signup
+)
 
 module.exports = router
